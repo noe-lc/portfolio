ebd29e0f6db8478d680fdb82c603947bfbd411df
Noe <nl@datalabmx.com>
2021-03-18 23:12:53 -0600

    tslib, generic set creator

--- src/utils/classification.ts
+++ src/utils/classification.ts
@@ -12,11 +12,27 @@ export const getUniqueValues: GetUniqueValues = <
   input: T[],
   field?: U
 ): T[] | T[U][] => {
+  let element;
+  const keys = {};
+  const set = [];
   const [first] = input;
 
   if (field && first[field]) {
-    return [first[field]];
-  } else {
-    return first;
+    for (const item of input) {
+      element = item[field];
+
+      if (typeof element !== 'string' && typeof element !== 'number') {
+        throw 'Cannot create sets of non-string, non-number values';
+      }
+
+      if (!keys[element]) {
+        keys[element as string] = 1;
+        set.push(element);
+      }
+    }
+
+    return set;
   }
+
+  return [...new Set(input)];
 };
